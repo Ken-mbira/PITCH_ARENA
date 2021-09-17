c976226d6feb7b170c2071a21f782d73121baf36
ken <ken.mbira@student.moringaschool.com>
2021-09-17 12:15:53 +0300

    change the app back to working status

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 class Config:
     """This is the main configuration class that has base configurations for the whole application
     """
-    pass
 
 class ProdConfig(Config):
     """This is the class containing the configurations needed for the production environment
@@ -19,7 +18,7 @@ class DevConfig(Config):
     Args:
         Config ([class]): [This is the parent class for the configurations]
     """
-    pass
+    DEBUG = True
 
 class TestConfig(Config):
     """This is the class containing the configurations needed for the testing environment

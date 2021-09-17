237ed8998f910f49fb2e40f77bcd04ed10daec82
ken <ken.mbira@student.moringaschool.com>
2021-09-17 13:14:45 +0300

    connected the app model to  the database pitch_arena

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     """This is the app factory function
     """
     app = Flask(__name__)
 
+    #setting up the configurations
     app.config.from_object(config_options[config_name])
 
-
+    #initializing the extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
 
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
     """This is the main configuration class that has base configurations for the whole application
     """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenmbira:1234@localhost/pitch_arena'
 
 class ProdConfig(Config):
     """This is the class containing the configurations needed for the production environment

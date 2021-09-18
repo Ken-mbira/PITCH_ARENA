7f0fce1a15758966e1252cd22bce517a77ac97ff
ken <ken.mbira@student.moringaschool.com>
2021-09-18 15:16:54 +0300

    add a new comment class and the link to the user class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_login import UserMixin
 
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -26,6 +27,7 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique=True,index=True)
     pass_secure = db.Column(db.String(255))
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
 
     @property
     def password(self):
@@ -52,3 +54,25 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Comment(UserMixin,db.Model):
+    """This will define all behaviours of the comments
+
+    Args:
+        UserMixin ([type]): [description]
+        db ([Model]): [This will connect all the comments to the database]
+    """
+    __tablename__ = 'comments'
+
+    comment_id = db.Column(db.Integer,primary_key=True)
+    comment_title = db.Column(db.String(255))
+    comment = db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        """This will save the comment to the database
+        """
+        db.session.add(self)
+        db.session.commit()

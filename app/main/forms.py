b255215c457ecc35898c7378cf16317029436ef1
ken <ken.mbira@student.moringaschool.com>
2021-09-20 19:09:58 +0300

    add a comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,RadioField
+from wtforms import StringField,SubmitField,RadioField,TextAreaField
 from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
@@ -11,3 +11,13 @@ class PitchForm(FlaskForm):
     pitch = StringField('Enter your pitch',validators=[Required()])
     pitch_category = RadioField('Enter the category of the pitch',validators=[Required()],choices=[('interview'),('promotion'),('Pickup lines'),('Comedy lines')])
     submit = SubmitField('Pitch')
+
+class CommentForm(FlaskForm):
+    """This will define all features of a comment form
+
+    Args:
+        FlaskForm ([type]): [description]
+    """
+    comment = TextAreaField('Comment here...',validators=[Required()])
+    vote = RadioField('Upvote or downvote',validators=[Required()],choices=[(1),(-1)])
+    submit = SubmitField('Comment')
--- app/models.py
+++ app/models.py
@@ -88,6 +88,8 @@ class Comment(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     comment = db.Column(db.String(255))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.pitch_id'))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
 
     def __repr__(self):
         return f'Comment {self.comment}'

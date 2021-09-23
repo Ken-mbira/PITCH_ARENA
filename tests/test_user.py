f6159cf5459f21e091a0eb4087b3e88dfcc8029c
ken <ken.mbira@student.moringaschool.com>
2021-09-23 12:05:29 +0300

    add a test to make sure the user is being instantiated correctly

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,8 @@ from app.models import User
 class UserModelTest(unittest.TestCase):
 
     def setUp(self):
+        """This will run before every test does
+        """
         self.new_user = User(password = 'banana')
 
     def test_password_setter(self):
@@ -15,3 +17,13 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('banana'))
+
+    def tearDown(self):
+        User.query.delete()
+
+    def test_instance(self):
+        """This will check if the user instance is being created correctly
+        """
+        self.user = User(username = 'Ken Mbira',email = 'mbiraaa@gmail.com')
+
+        self.assertEqual(self.user.username,'Ken Mbira')

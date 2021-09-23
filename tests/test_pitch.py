2578cd52d47ef2e11c3d51b4c2eb8c12e85a02a6
ken <ken.mbira@student.moringaschool.com>
2021-09-23 12:16:52 +0300

    add a test for the pitch to make sure that the pitches are being instantiated correctly

---
+++ tests/test_pitch.py
@@ -0,0 +1,20 @@
+import unittest
+
+from app.models import User,Pitch
+
+class PitchModelTest(unittest.TestCase):
+    """This will test all behaviours of the pitch model
+
+    Args:
+        unittest ([type]): [description]
+    """
+    def setUp(self):
+        """This will run before every test does
+        """
+        self.pitch = Pitch(pitch_category = 'Promotion pitch',pitch="This beats the life")
+
+    def test_instance(self):
+        """This will check if the pitch instance is being instantiated correctly
+        """
+        self.assertIsInstance(self.pitch,Pitch)
+        self.assertEqual(self.pitch.pitch_category,'Promotion pitch','The categories are not equal')
